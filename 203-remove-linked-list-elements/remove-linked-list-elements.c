852e1bedabfc94ece52bbef06da802c215bfc919
basavakumarcs24-hub <basavakumar.cs24@bmsce.ac.in>
2025-12-30 18:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (58.34%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -1,31 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 struct ListNode* removeElements(struct ListNode* head, int val) {
-    if (head == NULL)
-        return head;
 
-    while (head != NULL && head->val == val)
-        head = head->next;
-    if (head == NULL)
-        return NULL;
+    struct ListNode dummy;
+    dummy.next = head;
 
-    struct ListNode* trav = head;
-    while (trav->next != NULL)
-    {
-        if (trav->next->val == val)
-        {
-            trav->next = trav->next->next;
-        }
-        else
-        {
-            trav = trav->next;
+    struct ListNode* current = &dummy;
+
+    while (current->next != NULL) {
+        if (current->next->val == val) {
+            current->next = current->next->next;
+        } else {
+            current = current->next;
         }
     }
 
-    return head;
+    return dummy.next; 
 }
